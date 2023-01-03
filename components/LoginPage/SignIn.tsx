5377376aab5097028917b537c69e0864b06b4228
PranavK226 <69270517+PranavK226@users.noreply.github.com>
2023-01-02 17:52:26 -0800

    Seperating sign in and sign up

--- components/LoginPage/SignIn.tsx
+++ components/LoginPage/SignIn.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Button, Form } from "react-bootstrap";
 
 type Props = {
+    changePage: React.Dispatch<React.SetStateAction<string>>;
     signIn: (email: string, password: string) => void;
 }
 
-const SignIn: React.FC<Props> = ({ signIn }) => {
+const SignIn: React.FC<Props> = ({ changePage, signIn }) => {
     const [email, setEmail] = React.useState<string>("");
     const [password, setPassword] = React.useState<string>("");
 
@@ -19,30 +20,36 @@ const SignIn: React.FC<Props> = ({ signIn }) => {
     }
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group>
-                <Form.Label>
-                    <span><h2>Sign In<br /></h2></span>
-                </Form.Label>
-                <Form.Control
-                    type="email"
-                    className="input"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Enter email"
-                />
-                <Form.Control
-                    type="password"
-                    className="input"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Enter password"
-                />
-            </Form.Group>
-            <Button className="button bgsecondary my-3" type="submit">
-                Submit
-            </Button>
-        </Form>
+        <div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label>
+                        <span><h2>Sign In<br /></h2></span>
+                    </Form.Label>
+                    <Form.Control
+                        type="email"
+                        className="input"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter email"
+                    />
+                    <Form.Control
+                        type="password"
+                        className="input"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter password"
+                    />
+                </Form.Group>
+                <Button className="button bgsecondary my-3" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            <div>
+                Need an Account?
+                <button className="btn button bgprimary" onClick={() => changePage("signup")}>Sign Up</button>
+            </div>
+        </div>
     )
 }
 
--- components/LoginPage/SignUp.tsx
+++ components/LoginPage/SignUp.tsx
@@ -3,14 +3,15 @@ import { Button, Form } from "react-bootstrap";
 import ListGroup from "react-bootstrap/ListGroup";
 
 type Props = {
+    changePage: React.Dispatch<React.SetStateAction<string>>;
     signUp: (email: string, password: string) => void;
 }
 
-const SignUp: React.FC<Props> = ({ signUp }) => {
+const SignUp: React.FC<Props> = ({ changePage, signUp }) => {
     const [email, setEmail] = React.useState<string>("");
     const [password, setPassword] = React.useState<string>("");
     const [repeatPassword, setRepeat] = React.useState<string>("");
-
+    
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (!email) return;
@@ -23,45 +24,51 @@ const SignUp: React.FC<Props> = ({ signUp }) => {
     }
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group>
-                <Form.Label>
-                    <span><h2>Sign Up<br /></h2></span>
-                </Form.Label>
-                <Form.Control
-                    type="email"
-                    className="input"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Enter email"
-                />
-                <Form.Control
-                    type="password"
-                    className="input"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Enter password"
-                />
-                <Form.Control
-                    type="password"
-                    className="input"
-                    value={repeatPassword}
-                    onChange={(e) => setRepeat(e.target.value)}
-                    placeholder="Repeat Password"
-                />
-                <ListGroup className="my-2">
-                    <ListGroup.Item className={password.length < 8 ? "danger " : "success "}>
-                        {password.length < 8 ? "Password needs to be more than 8 letters" : "Password is more than 8 letters"}
-                    </ListGroup.Item>
-                    <ListGroup.Item className={password != repeatPassword ? "danger " : "success "}>
-                        {password != repeatPassword ? "Passwords don't match" : "Passwords are matching"}
-                    </ListGroup.Item>
-                </ListGroup>
-            </Form.Group>
-            <Button className="button bgsecondary my-3" type="submit">
-                Submit
-            </Button>
-        </Form>
+        <div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label>
+                        <span><h2>Sign Up<br /></h2></span>
+                    </Form.Label>
+                    <Form.Control
+                        type="email"
+                        className="input"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter email"
+                    />
+                    <Form.Control
+                        type="password"
+                        className="input"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter password"
+                    />
+                    <Form.Control
+                        type="password"
+                        className="input"
+                        value={repeatPassword}
+                        onChange={(e) => setRepeat(e.target.value)}
+                        placeholder="Repeat Password"
+                    />
+                    <ListGroup className="my-2">
+                        <ListGroup.Item className={password.length < 8 ? "danger " : "success "}>
+                            {password.length < 8 ? "Password needs to be more than 8 letters" : "Password is more than 8 letters"}
+                        </ListGroup.Item>
+                        <ListGroup.Item className={password != repeatPassword ? "danger " : "success "}>
+                            {password != repeatPassword ? "Passwords don't match" : "Passwords are matching"}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Form.Group>
+                <Button className="button bgsecondary my-3" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            <div>
+                Already have an Account?
+                <button className="btn button bgprimary" onClick={() => changePage("signin")}>Sign In</button>
+            </div>
+        </div>
     )
 }
 
