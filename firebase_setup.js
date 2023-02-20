9ddad3076056be5873595e907d35ca301b6f5a6e
PranavK226 <69270517+PranavK226@users.noreply.github.com>
2023-02-19 23:50:34 -0800

    login fix

--- firebase_setup.js
+++ firebase_setup.js
@@ -20,4 +20,4 @@ export const auth = firebase.auth();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithRedirect(provider)
+export const signInWithGoogle = () => auth.signInWithPopup(provider)
