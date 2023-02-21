3fb66f4c0f63d7a4ef9e3ec7e4f3ca5de9b03b82
PranavK226 <69270517+PranavK226@users.noreply.github.com>
2023-02-20 20:22:35 -0800

    reset password page

--- components/LoginPage/ResetPassword.tsx
+++ components/LoginPage/ResetPassword.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
 import { auth } from "../../firebase_setup";
+import Image from "next/image"
 
 type Props = {
     changePage: React.Dispatch<React.SetStateAction<string>>;
@@ -19,31 +20,39 @@ const ResetPassword: React.FC<Props> = ({ changePage }) => {
             alert(error)
         }
         setEmail("")
+        changePage("signin")
     }
 
     return (
-        <div className="grad">
+        <div className="row login">
+            <title>
+                Sign Up
+            </title>
             <div className="bgdark sign">
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group>
-                        <Form.Label>
-                            <h2 className="primary">Sign In<br /></h2>
-                        </Form.Label>
-                        <Form.Control
-                            required={true}
-                            type="email"
-                            className="input"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="Enter email"
-                        />
-                    </Form.Group>
-                    <Button className="button-primary bgprimary my-3" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-                <hr />
-                <Button className="button-primary bgprimary" onClick={() => changePage("signin")}>Return to Sign In</Button>
+                <div className="content">
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group>
+                            <Form.Label>
+                                <h2 className="primary">Sign In<br /></h2>
+                            </Form.Label>
+                            <Form.Control
+                                required={true}
+                                type="email"
+                                className="input"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder="Enter email"
+                            />
+                        </Form.Group>
+                        <Button className="button-primary bgprimary my-3" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                    <a className="light" onClick={() => changePage("signin")}>Return to Sign In</a>
+                </div>
+            </div>
+            <div className="imageContainer">
+                <Image className="loginImage" src="/images/forgotpassword.svg" width={32} height={32} alt="Forgot Password"/>
             </div>
         </div>
     )
